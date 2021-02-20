7d77208c164d5cd9b9b281dbd4544eccaec4b2cc
jarquafelmu <thechaoselement@gmail.com>
2021-02-20 15:22:07 -0700

    create customer

--- app/controllers/customer.controller.js
+++ app/controllers/customer.controller.js
@@ -1,7 +1,31 @@
 const Customer = require("../models/customer.model.js");
 
 // Create and Save a new Customer
-exports.create = (req, res) => {};
+exports.create = (req, res) => {
+  // Validate request
+  if (!req.body) {
+    res.status(400).send({
+      message: `Content cannot be empty!`
+    });
+  }
+
+  // Create  a customer
+  const customer = new Customer({
+    email: req.body.email,
+    name: req.body.name,
+    active: req.body.active
+  });
+
+  // Save Customer in the database
+  Customer.create(customer, (err, data) => {
+    if (err)
+      return res.status(500).send({
+        message:
+          err.message || `Some error occured while creating the Customer.`
+      });
+    res.send(data);
+  });
+};
 
 // Retreive all Customers from the database
 exports.findAll = (req, res) => {};
