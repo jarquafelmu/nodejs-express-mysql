7398223bc4b7e944cda67a6a045b6f46955a540b
jarquafelmu <thechaoselement@gmail.com>
2021-02-20 14:36:01 -0700

    moved location

--- server.js
+++ server.js
@@ -1,22 +1,23 @@
 const express = require("express");
-const bodyPaser = require("body-parser");
+const bodyParser = require("body-parser");
 
 const app = express();
 
 // parse requests of content-type: application/json`
-app.use(bodyPaser.json());
+app.use(bodyParser.json());
 
 // parse requets of content-type: application/x-www-form-uriencoded
-app.use(bodyPaser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // simple route
 app.get(`/`, (req, res) => {
   res.json({ message: `Welcome to jarquafelmu application.` });
 });
-``
+
 // set port, listen for request
 // change port to 8080 before uploading to Elastic Beanstalk
 const port = 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+``;
