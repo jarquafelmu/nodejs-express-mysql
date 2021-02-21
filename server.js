a400c8fba98a67346cb37e9badb3365363e0967f
jarquafelmu <thechaoselement@gmail.com>
2021-02-20 17:38:24 -0700

    added cors handling

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const bodyParser = require("body-parser");
 
 const app = express();
+
+app.use(cors());
 
 // parse requests of content-type: application/json`
 app.use(bodyParser.json());
